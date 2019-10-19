6defedea6938dd5f4ea84360121ce0370d9e1b08
Moody Rahman <mrahman01@stuy.edu>
2019-10-19 11:05:00 -0400

    database file

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask import session
+import sqlite3   #enable control of an sqlite database
 
 app = Flask(__name__)
+
+DB_FILE="data.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
 
 @app.route('/')
 def hello():
