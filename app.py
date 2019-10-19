59389902cdd4e4b2b12ca995dd6df68760c1e1b1
Moody Rahman <mrahman01@stuy.edu>
2019-10-19 16:42:36 -0400

    registration

--- app.py
+++ app.py
@@ -8,16 +8,39 @@ import sqlite3   #enable control of an sqlite database
 
 app = Flask(__name__)
 
-DB_FILE="data.db"
-
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()               #facilitate db ops
 
 @app.route('/')
 def hello():
     print(__name__)
-    return 'reeeee'
+    return render_template("appuserdebug.html")
+
+
+@app.route('/register')
+def register():
+	if len(request.args) == 0:
+		return render_template("register.html")
+	else:
+		command = "INSERT INTO users VALUES('{}','{}');".format(request.args["usernamein"], request.args["passwordin"])
+		runsqlcommand(command)
+		return "you are now registered!!"
+
+
+
+def runsqlcommand(command):
+	DB_FILE="data.db"
+	db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+	c = db.cursor()               #facilitate db ops
+	c.execute(command)
+	if "select" in command.lower():
+		return c.fetchall()
+	db.commit() #save changes
+	db.close()  #close database
+
+
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
+# z = runsqlcommand("INSERT INTO users VALUES('Richard','Mutt');")
+# print(z)
