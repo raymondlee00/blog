de15aca6b8dba119dd61bbacbd1d025be66b4081
Alex  Olteanu <alexolteanu@Alexs-MacBook-Pro-2.local>
2019-10-22 18:40:38 -0400

    updated database

--- sqldb.py
+++ sqldb.py
@@ -4,22 +4,25 @@ import csv       #facilitate CSV I/O
 DB_FILE= "glit.db"
 db = sqlite3.connect("glit.db") #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
+
 #==============================================================================
-#create table to store userinformation
+
+#create table to store user information
 #row[0] = username
 #row[1] = password
-command = "CREATE TABLE userinfo(username TEXT, password TEXT);"
-c.execute(command) # run SQL statement
+#command = "CREATE TABLE userinfo(username TEXT, password TEXT);"
+#c.execute(command) # run SQL statement
 
-#create table to store blog posts
+#create table to store blog information
 #row[0] = username of blog creator
 #row[1] = blogid
 #row[2] = blog title
 #row[3] = blog content
-command = "CREATE TABLE bloginfo(username TEXT, blogid INTEGER, title TEXT, content TEXT);"
-c.execute(command) # run SQL statement
+#command = "CREATE TABLE bloginfo(username TEXT, blogid INTEGER, title TEXT, content TEXT);"
+#c.execute(command) # run SQL statement
 
 #==============================================================================
+
 #USERINFO TABLE COMMANDS
 #returns password for a given username, if username is nonexistant return None
 def fetchPassword(username):
@@ -29,7 +32,7 @@ def fetchPassword(username):
     for row in data:
          if row[0] == username:
             return row[1]
-        else:
+         else:
             return None
 
 #adds a user to the userinfo table
@@ -39,18 +42,25 @@ def addUser(username, password):
     data = c.fetchall()
 
 #______________________________________________________________________________
+
 #BLOGINFO TABLE COMMANDS
 #returns blog title given its blogid
 def fetchBlogTitle(blogid):
     command = "SELECT * FROM bloginfo"
     c.execute(command)
     data = c.fetchall()
+    for row in data:
+        if row[1] == blogid:
+            return row[2]
 
 #returns blog content given its blogid
 def fetchBlogContent(blogid):
     command = "SELECT * FROM bloginfo"
     c.execute(command)
     data = c.fetchall()
+    for row in data:
+        if row[1] == blogid:
+            return row[3]
 
 #returns all blog titles
 def fetchAllBlogTitles():
@@ -66,6 +76,13 @@ def fetchAllBlogContent():
 
 #adds a post to the blogs table
 def addBlog(username, blogid, title, content):
+    command = "INSERT INTO bloginfo(username, blogid, title, content) VALUES" + "(" + "'" + str(username) + "'" + "," + str(blogid) + "," + title + "," + content + ");"
+    c.execute(command)
 
 #deletes a blog from the blogs table
 def deleteBlog(blogid):
+    command = "SELECT * FROM bloginfo"
+    c.execute(command)
+    data = c.fetchall()
+
+#addBlog("bruh", 1, "tre", "ddf")
