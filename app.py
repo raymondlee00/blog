2a6fdff17eb354535c5710d912e487a8966dfe08
athompson00 <athompson00@stuy.edu>
2019-10-27 03:38:40 -0400

    added search capabilities

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import url_for
 from flask import session
 from flask import flash
 import time
-import sqlite3   #enable control of an sqlite database
+import sqlite3  # enable control of an sqlite database
 import sqldb
 
 
@@ -14,119 +14,142 @@ app = Flask(__name__)
 app.secret_key = 'hfjkafhrku'
 @app.route('/test')
 def test():
-	print(__name__)
-	return render_template("results.html")##For html testing purposes
+    print(__name__)
+    return render_template("results.html")  # For html testing purposes
 
 
 @app.route('/')
 def hello():
-	print(__name__)
-	if "username" in session:
-		return redirect("/welcome")
-	return render_template("login.html")
+    print(__name__)
+    if "username" in session:
+        return redirect("/welcome")
+    return render_template("login.html")
 
 
 @app.route('/debug')
 def hddd():
-	print(__name__)
-	return session["username"]
+    print(__name__)
+    return session["username"]
 
-#REdirect to hmer
+# REdirect to hmer
 @app.route('/register')
 def register():
-	if len(request.args) == 0:
-		return render_template("register.html")
-	else:
-		existencecommand = "SELECT * FROM userinfo WHERE username = '{}'".format(request.args["usernamein"])
-		if(len(runsqlcommand(existencecommand)) == 0):
-			command = "INSERT INTO userinfo VALUES('{}','{}');".format(request.args["usernamein"], request.args["passwordin"])
-			runsqlcommand(command)
-			session["usernamein"] = request.args["passwordin"]
-			return redirect("/")
-		else:
-			flash("username already exists")
-			return redirect("/register")
+    if len(request.args) == 0:
+        return render_template("register.html")
+    else:
+        existencecommand = "SELECT * FROM userinfo WHERE username = '{}'".format(
+            request.args["usernamein"])
+        if(len(runsqlcommand(existencecommand)) == 0):
+            command = "INSERT INTO userinfo VALUES('{}','{}');".format(
+                request.args["usernamein"], request.args["passwordin"])
+            runsqlcommand(command)
+            session["usernamein"] = request.args["passwordin"]
+            return redirect("/")
+        else:
+            flash("username already exists")
+            return redirect("/register")
 
 
 @app.route('/auth')
 def auth():
-	if len(request.args) == 0:
-		session.pop("username")
-		return redirect("/")
+    if len(request.args) == 0:
+        session.pop("username")
+        return redirect("/")
 
-	command = "SELECT * FROM userinfo where username = '{}'".format(request.args["username"])
-	pair = runsqlcommand(command)
-	if len(pair) == 0:
-		return render_template("login.html", error = "Your username is unfortunately WRONG")
-	else:
-		if (request.args["password"] == pair[0][1]):
-			session["username"] = request.args["username"]
-			return redirect("/welcome")
-		else:
-			return render_template("login.html", error = "Your password is unfortunately WRONG")
+    command = "SELECT * FROM userinfo where username = '{}'".format(
+        request.args["username"])
+    pair = runsqlcommand(command)
+    if len(pair) == 0:
+        return render_template("login.html", error="Your username is unfortunately WRONG")
+    else:
+        if (request.args["password"] == pair[0][1]):
+            session["username"] = request.args["username"]
+            return redirect("/welcome")
+        else:
+            return render_template("login.html", error="Your password is unfortunately WRONG")
+
 
 @app.route('/welcome')
 def welcome():
-	command = "SELECT * FROM bloginfo where username = '{}'".format(session["username"])
-	rawdat = runsqlcommand(command)
-	rawdat.reverse()
-	allposts = {}
-	for x in rawdat:
-		allposts[x[1]] = x[2]
-	# for x in revkey:
-	# 	allpostsrev[revkey] = allposts[revkey]
-	# print(allpostsrev)
-	return render_template("welcome.html", username = session["username"], posts = allposts)
+    command = "SELECT * FROM bloginfo where username = '{}'".format(
+        session["username"])
+    rawdat = runsqlcommand(command)
+    rawdat.reverse()
+    allposts = {}
+    for x in rawdat:
+        allposts[x[1]] = x[2]
+    # for x in revkey:
+    # 	allpostsrev[revkey] = allposts[revkey]
+    # print(allpostsrev)
+    return render_template("welcome.html", username=session["username"], posts=allposts)
 
 
 @app.route('/addpost')
 def postadd():
-	title = request.args["postTitle"]
-	content = request.args["postContent"]
-	command = "INSERT INTO bloginfo VALUES('{}','{}', '{}')".format(session["username"], title, content)
-	runsqlcommand(command)
-	flash("added post alright")
-	return redirect("/welcome")
+    title = request.args["postTitle"]
+    content = request.args["postContent"]
+    command = "INSERT INTO bloginfo VALUES('{}','{}', '{}')".format(
+        session["username"], title, content)
+    runsqlcommand(command)
+    flash("added post alright")
+    return redirect("/welcome")
+
 
 @app.route("/delete")
 def delete():
-	entrytodelete = request.args["postTitle"]
-	command = "DELETE FROM bloginfo WHERE title = '{}'".format(entrytodelete)
-	runsqlcommand(command)
-	return redirect("/welcome")
+    entrytodelete = request.args["postTitle"]
+    command = "DELETE FROM bloginfo WHERE title = '{}'".format(entrytodelete)
+    runsqlcommand(command)
+    return redirect("/welcome")
 
 
 @app.route('/search')
 def search():
-	return render_template("search.html")
+    return render_template("search.html")
+
 
 @app.route('/results')
 def results():
-	return render_template("results.html")
+    searchInput = request.args["searchInput"].lower()
+    length = len(searchInput)
+    command = "SELECT title, content FROM bloginfo"
+    allPosts = runsqlcommand(command)
+    results = {}
+    for post in allPosts:
+        title = post[0]
+        content = post[1]
+        titlecomp = title.lower()
+        if (titlecomp.__contains__(searchInput)):
+            results[title] = content
+
+    return render_template("results.html", results=results)
+
 
 @app.route('/createPost')
 def createPost():
-	return render_template("createPost.html")
+    return render_template("createPost.html")
+
 
 @app.route('/showall')
 def showall():
-	command = "SELECT username FROM userinfo"
-	allusers = runsqlcommand(command)
-	print(type(allusers[0]))
-	print(allusers)
-	return render_template("showall.html", usernames = allusers)
+    command = "SELECT username FROM userinfo"
+    allusers = runsqlcommand(command)
+    print(type(allusers[0]))
+    print(allusers)
+    return render_template("showall.html", usernames=allusers)
+
 
 def runsqlcommand(command):
-	DB_FILE="glit.db"
-	db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-	c = db.cursor()               #facilitate db ops
-	c.execute(command)
-	if "select" in command.lower():
-		return c.fetchall()
-	db.commit() #save changes
-	db.close()  #close database
+    DB_FILE = "glit.db"
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+    c.execute(command)
+    if "select" in command.lower():
+        return c.fetchall()
+    db.commit()  # save changes
+    db.close()  # close database
 
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
